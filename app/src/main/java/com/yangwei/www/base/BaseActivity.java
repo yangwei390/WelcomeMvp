d65a2a6e865472146319342f8932124a1843e365
yangwei390 <lnywyq@126.com>
2017-08-15 14:12:27 +0800

    基类

---
+++ app/src/main/java/com/yangwei/www/base/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.yangwei.www.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by yangwei on 2017/8/15.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/yangwei/www/base/BaseFragment.java
@@ -0,0 +1,10 @@
+package com.yangwei.www.base;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by yangwei on 2017/8/15.
+ */
+
+public class BaseFragment extends Fragment {
+}
