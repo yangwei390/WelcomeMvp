1a480e2b5a5be081f46a7ae45a3e29720329df60
yangwei <yangwei390@163.com>
2018-05-31 17:24:55 +0800

    MVP完善

--- app/src/main/java/com/yangwei/www/base/BaseFragment.java
+++ app/src/main/java/com/yangwei/www/base/BaseFragment.java
@@ -8,10 +8,20 @@ import com.yangwei.www.utils.CommonUiTools;
  * Created by yangwei on 2017/8/15.
  */
 
-public class BaseFragment extends Fragment {
+public class BaseFragment extends Fragment implements IBaseView {
 
     public void showLoginErrDialog() {
         toast("登录异常");
+    }
+
+    @Override
+    public void showNetDialog() {
+
+    }
+
+    @Override
+    public void dismissNetDialog() {
+
     }
 
     public void toast(String str) {
